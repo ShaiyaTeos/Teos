e9ce5729d5ef5f142117e661ca77fa883d11c021
Cups <cups@shaiya.net>
2021-04-18 19:31:43 +0800

    feat(Client): Identify function to get a player's local address for their id.

--- client/asm/player.asm
+++ client/asm/player.asm
@@ -3,3 +3,4 @@ player_map  equ 0x8BEB64    ; The address where the user's current map id is sto
 pos_x       equ 0x8B5444    ; The address of the player's x position
 pos_y       equ 0x8B5448    ; The address of the player's y position
 pos_z       equ 0x8B544C    ; The address of the player's z position
+get_player  equ 0x4498C0    ; The address for getting a player for their id.
